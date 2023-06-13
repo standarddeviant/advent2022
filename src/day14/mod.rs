4e2da43ab576042376ca67231e36ea451026bc15
standarddeviant <dave.crist@gmail.com>
2023-06-13 00:39:13 -0400

    day14, part2, non-optimal but correct, finishes in ~86 seconds

--- src/day14/mod.rs
+++ src/day14/mod.rs
@@ -6,6 +6,9 @@ pub fn run(fname: &str) {
     let start = Instant::now();
     part1(fname);
     println!("part1 took {:?} seconds", start.elapsed());
+
+    // TODO - see if there is a "timeit" function in rust
+    part2(fname);
 }    
 
 fn part1(fname: &str) {
@@ -18,6 +21,25 @@ fn part1(fname: &str) {
     }
     println!("part1: placed_sand count = {}", f.count);
 }
+
+fn part2(fname: &str) {
+    let mut f = parse_file(fname);
+    let mut ix = 0;
+    let start = Instant::now();
+    loop {
+        match f.place_sand(false) {
+            Some(_xy) => {        },
+            None          => { break; }
+        }
+        if ix % 1000 == 0 {
+            println!("DBG: ix = {ix} @ {:?}", start.elapsed());
+        }
+        ix += 1;
+    }
+    println!("part2: placed_sand count = {}", f.count);
+    println!("part2 took {:?} seconds", start.elapsed());
+}
+
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 struct XY { x: i32, y: i32 }
@@ -96,6 +118,7 @@ struct Formation {
 
 impl Formation {
     fn place_sand(&mut self, p1: bool) -> Option<XY> {
+        let p2 = !p1;
         /* send location */
         'outer: loop {
             /* check if the path is totally empty, i.e., that we have
@@ -104,26 +127,74 @@ impl Formation {
             if self.path.is_empty() { return None; }
 
             /* set current start pos from last point in path */
-            let s = self.path[self.path.len() - 1];
+            let mut s = self.path[self.path.len() - 1].clone();
 
-            /* return None if this sand has fallen into the void */
-            if s.y >= self.yvoid && p1 { return None; }
+            /* if p1: return None if this sand has fallen into the void */
+            // TODO - make p1 a struct var?
+            if p1 && s.y >= self.yvoid { return None; }
+
+            /*
+            if p2 {
+                if s.y == self.yvoid + 1 { 
+                    if !self.fill.contains(&s) {
+                        self.fill.push(s);
+                    }
+                    self.path.pop();
+                    continue;
+                }
+            }
+            */
 
             /* look for free space in order of dn, dnleft, dnright   */
-            let possible_empty_spaces = [
-                &XY{x: s.x  , y: s.y+1}, // dn
-                &XY{x: s.x-1, y: s.y+1}, // dnleft
-                &XY{x: s.x+1, y: s.y+1}  // dnright
+            let spaces: Vec<XY> = vec![
+                XY{x: s.x  , y: s.y+1}, // dn
+                XY{x: s.x-1, y: s.y+1}, // dnleft
+                XY{x: s.x+1, y: s.y+1}  // dnright
             ];
-            for space in possible_empty_spaces {
-                if !self.fill.contains(space) {
+            // println!("\nspaces = {spaces:?}");
+            let spaces: Vec<XY> = spaces.into_iter().filter(|a| a.y <= 1+self.yvoid).collect();
+            // println!("spaces = {spaces:?}");
+            // .iter().filter(|a| a.y <= self.yvoid).collect();
+
+            /*
+            if s.x == 500 && s.y ==  7 {
+                return None;
+            }
+            */
+
+            // println!("testing s = {s:?}");
+            // println!("path.len() = {:?}", self.path.len());
+            for space in &spaces {
+                // println!("self.fill.contains({space:?}) = {:?}", self.fill.contains(&space));
+                if !self.fill.contains(&space) {
+                    // println!("PUSHING SPACE TO PATH: {space:?}");
                     self.path.push(*space);
                     continue 'outer;
                 }
             }
 
+            /* 
+            if p2 {
+                let all_filled = 
+                    (&spaces).clone().iter()
+                    .all(|a| self.fill.contains(a));
+                if all_filled {
+                    // println!("ALL FILLED @ {s:?}, yvoid={}", self.yvoid);
+                    // println!("self.path.len() = {}", self.path.len());
+                    // println!("self.path = {:?}", self.path);
+                    self.path.pop();
+                    self.fill.push(s);
+                    self.count += 1;
+                    // println!("self.path.len() = {}", self.path.len());
+                    // println!("self.path = {:?}", self.path);
+                    continue;
+                }
+            }
+            */
+
             /* getting here means we can place sand @ s, b/c self.fill already
                contains dn, dnleft, and dnright */
+            // println!("pushing {s:?}");
             self.fill.push(s); /* push XY to fill  */
             self.path.pop();   /* pop XY from path */
             self.count += 1;   /* increase count   */
@@ -155,7 +226,14 @@ impl str::FromStr for Formation {
         let yvoid: i32 = fill[fill.len() - 1].y;
 
         let start_xy = XY { x: 500, y: 0 };
-        return Ok(Formation{path: vec![start_xy], fill: fill, count: 0, yvoid: yvoid });
+        return Ok(
+            Formation{
+                path: vec![start_xy],
+                fill: fill,
+                count: 0,
+                yvoid: yvoid
+            }
+        );
     }
 }
 
